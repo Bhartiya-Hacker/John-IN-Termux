b05a0d1718cbe9a8bbb9ed79540d6e7cbf155781
Nitin.kumar <108232080+Bhartiya-Hacker@users.noreply.github.com>
2023-01-05 22:12:11 +0530

    Update john.sh

--- john.sh
+++ john.sh
@@ -9,30 +9,30 @@ BLUE='\033[0;34m'
 LIGHTBLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
-echo -e "${YELLOW}Checking if all package are updated or not ${NC}"
+echo -e "${YELLOW}Checking if System is updated or not ${NC}"
 cd;pkg update -y
-echo -e "${GREEN}Installing dependencies${NC}"
+echo -e "${ORANGE}Installing dependencies${NC}"
 pkg install git clang perl build-essential make -y
-echo -e "${GREEN}Started cloning core project${NC}"
+echo -e "${ORANGE}Started cloning John-the-Ripper${NC}"
 git clone git://github.com/magnumripper/JohnTheRipper -b bleeding-jumbo john
-echo -e "${BLUE}Navigating to the main folder${NC}"
+echo -e "${BLUE}Navigating to the Configuration folder${NC}"
 cd john/src/
 clear
-echo -e "
+echo -e "${LIGHTBLUE}
     +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
      	    |J|O|H|N|  |i|n|  |T|e|r|m|u|x|
     +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
            +-+-+ +-+-+-+-+-+-+-+-+-+-+
               |b|y| |BHARTIYA HACKER|
            +-+-+ +-+-+-+-+-+-+-+-+-+-+
-"
-echo -e "${GREEN}Started configuration, It may took some time${NC}"
+${NC}"
+echo -e "${ORANGE}Started configuration, It may took some time${NC}"
 sleep 0.1
-echo -e "${GREEN}Configuring core engine, please wait ...${NC}"
+echo -e "${ORANGE}Configuring core engine, please wait ...${NC}"
 ./configure
 clear
-echo -e "${GREEN}Configuration Completed. Now starting final proccess it will took approximately 5-10 min"
-echo -e "Started final process, please wait...${NC}"
+echo -e "${ORANGE}Configuration Completed. Now starting final proccess it will took approximately 5-10 min"
+echo -e "${LIGHTRED}Started final process... please wait...${NC}"
 make -s clean && make -sj4
 cd ../../
-echo -e "${GREEN}Process completed. You can check it by giving the following command ${LIGHTBLUE}./john/run/john --help${NC}"
+echo -e "${RED}Process completed (~_*) : You can check it by giving the following command ${LIGHTBLUE}./john/run/john --help${NC}"
